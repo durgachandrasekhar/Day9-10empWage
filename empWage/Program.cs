6915865efc86ba9186170c388a087a19cad43274
durgachandrasekhar <durgachandrasekhar3@gmail.com>
2021-08-13 07:52:20 +0530

    feat:Emplooyee wage for multiple companies

---
+++ empWage/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmpWage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWage.EmployeeClass obj1 = new EmpWage.EmployeeClass();
+            obj1.CompanyEmpWage("CTS", 57, 27, 100);
+            obj1.CompanyEmpWage("TCS", 77, 30, 150);
+        }
+    }
+}
